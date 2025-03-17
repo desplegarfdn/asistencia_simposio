d1638929768fe651d149ec3e2cf49ba3f5366127
eduardo_orozco <eduardo.orozco66@unach.mx>
2025-03-17 03:47:17 -0600

    v1.3.2

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        base: '/', // 🔹 Importante para Render y evitar errores 404 en rutas
+
         plugins: [vue()],
+
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
             }
         },
+
         build: {
-            chunkSizeWarningLimit: 2000, // Ajusta el límite a 2000 KB (2 MB) o el valor que desees
+            outDir: 'dist', // 🔹 Render usa `dist/` como carpeta pública
+            chunkSizeWarningLimit: 2000 // Ajusta el límite a 2000 KB (2 MB)
+        },
+
+        server: {
+            port: 3000, // Opcional: Puedes cambiarlo si es necesario
+            historyApiFallback: true // 🔹 Evita errores 404 en rutas anidadas
         }
     };
 });
